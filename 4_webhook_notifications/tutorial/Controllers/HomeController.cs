904c9b6bbbc246967776431b9d1320207990a171
Braintree <code@getbraintree.com>
2013-08-16 14:52:55 +0000

    Add webhook guide

---
+++ 4_webhook_notifications/tutorial/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Braintree;
+
+namespace braintree_tutorial.Controllers
+{
+    public class Constants
+    {
+        public static BraintreeGateway Gateway = new BraintreeGateway
+        {
+            Environment = Braintree.Environment.DEVELOPMENT,
+            MerchantId = "integration_merchant_id",
+            PublicKey = "integration_public_key",
+            PrivateKey = "integration_private_key"
+        };
+    }
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCustomer(FormCollection collection)
+        {
+            CustomerRequest request = new CustomerRequest
+            {
+                FirstName = collection["first_name"],
+                LastName = collection["last_name"],
+                CreditCard = new CreditCardRequest
+                {
+                    BillingAddress = new CreditCardAddressRequest
+                    {
+                        PostalCode = collection["postal_code"]
+                    },
+                    Number = collection["number"],
+                    ExpirationMonth = collection["month"],
+                    ExpirationYear = collection["year"],
+                    CVV = collection["cvv"]
+                }
+            };
+
+            Result<Customer> result = Constants.Gateway.Customer.Create(request);
+            if (result.IsSuccess())
+            {
+                Customer customer = result.Target;
+                ViewData["CustomerName"] = customer.FirstName + " " + customer.LastName;
+                ViewData["CustomerId"] = customer.Id;
+            }
+            else
+            {
+                ViewData["Message"] = result.Message;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ 4_webhook_notifications/tutorial/Controllers/WebhooksController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Braintree;
+using System.Diagnostics;
+
+namespace braintree_tutorial.Controllers
+{
+    public class WebhooksController : Controller
+    {
+        public ActionResult Accept()
+        {
+            if (Request.HttpMethod == "POST")
+            {
+                WebhookNotification webhookNotification = Constants.Gateway.WebhookNotification.Parse(
+                    Request.Params["bt_signature"],
+                    Request.Params["bt_payload"]
+                    );
+
+                string message = string.Format(
+                   "[Webhook Received {0}] | Kind: {1} | Subscription: {2}",
+                    webhookNotification.Timestamp.Value,
+                    webhookNotification.Kind,
+                    webhookNotification.Subscription.Id
+                    );
+
+                System.Console.WriteLine(message);
+                return new HttpStatusCodeResult(200);
+            }
+            else
+            {
+                return Content(Constants.Gateway.WebhookNotification.Verify(Request.QueryString["bt_challenge"]));
+            }
+            
+        }
+    }
+}
